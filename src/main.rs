6d9abc3dd16098746e3429896b989a2139e5456f
ailton teles xavier <atxshared@gmail.com>
2025-04-29 18:02:14 -0300

    gerando numero aleatorio e comparando

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
+use std::cmp::Ordering;
 use std::io;
+use rand::Rng;
 
 fn main() {
     print!("Guess the number!");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    //println!("The secret number is: {secret_number}");
+
     println!("Please input you guess.");
 
     let mut guess = String::new();
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    print!("You guessed: {}", guess);
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
+    println!("You guessed: {} ", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You wind!"),
+        
+    }
 
 }
+
