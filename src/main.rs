6ef7e455302262d448de1540a525804294540b4e
ailton teles xavier <atxshared@gmail.com>
2025-04-30 12:55:59 -0300

    ..

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
@@ -9,22 +10,31 @@ fn main() {
 
     //println!("The secret number is: {secret_number}");
 
-    println!("Please input you guess.");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {} ", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You wind!"),
+    loop {
+        println!("Please input you guess.");
         
+        let mut guess = String::new();
+    
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {} ", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You wind!");
+                break;
+            }
+            
+        }
     }
+
 
 }
 
